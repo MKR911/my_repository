e41a1ecc9364f8493221a546d75edc36521fbd9e
Mkr <mayankrajputmkr911@gmail.com>
2024-01-12 00:05:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+let pencil = 10;
+let eraser = 5;
+console.log("total price is:", pencil + eraser, "rupees.");
+console.log(`total price is: ${pencil + eraser} rupees.`);
+
+alert("hwwwwwwwwwwwww.......");
+console.error("chal bey");
+console.warn("sambhaal ke kake");
+
+
+let a = prompt("enter no. 1: ");
+let b = prompt("enter no. 2: ");
+let put = a + b;
+console.log(`total is: ${put}`);
+
+if ('A' == 'a') {
+    console.log("ho ra hai bhai.");
+}
+
+
+// let todo = [];
+
+// let req = prompt("please enter your request");
+
+// while (true) {
+//     if (req == "quit") {
+//         console.log("quitting app");
+//         break;
+//     }
+
+//     if (req == "list") {
+//         console.log("----------------");
+//         for (let i = 0; i < todo.length; i++) {
+//             console.log(i, todo[i]);
+//         }
+//         console.log("----------------");
+//     }
+//     else if(req == "add"){
+//         let task = prompt("please enter the task you want to add");
+//         todo.push(task);
+//         console.log("task added");
+//     }
+//     else if(req == "delete"){
+//         let idx = prompt("please enter the task index");
+//         todo.splice(idx,1);
+//         console.log("task deleted");
+//     }
+
+//     req = prompt("please enter your request");
+// }
+
+// let arr = [1,2,3,4,5,6,2,3];
+// let num = prompt("Enter the key: ");
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]  );
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+    
+//     if (num == arr[i]) {
+//         arr.splice(i,1);
+//     }
+    
+// }
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]  );
+// }
+
+
+// let num = prompt("Enter number: ");
+// let count = 0;
+
+// while (num > 0) {
+//     num = Math.floor(num/10);
+//     count++;
+// }
+
+// console.log(count);
